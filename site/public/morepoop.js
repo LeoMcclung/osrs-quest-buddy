34dc46547b4e25a48068befc0745698074f29397
xaishinn <murraythemoose@gmail.com>
2017-01-16 20:07:26 -0700

    More poop
    this time because of Charlie

---
+++ site/public/morepoop.js
@@ -0,0 +1,9 @@
+function shitagain(){
+    var password=prompt("Password?");
+    if(password==="banerules") {
+	      alert("Youre cool but the sites not ready yet, sorry")
+     }
+    else {
+	      alert("Lex Luthor says 'WROOOOOONG'");
+     }
+}
